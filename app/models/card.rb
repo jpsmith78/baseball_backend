143f7012efac4103c7cd983be8cbf1305a793d8c
Carlos Godoy <cgod720@gmail.com>
2019-03-02 12:04:04 -0500

    card model and controller self all added

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,1 +1,24 @@
+class Card
 
+  DB = PG.connect({
+    :host =>'localhost',
+    :port =>5432,
+    :dbname =>"baseball_backend_development"
+    })
+
+    def self.all
+      results = DB.exec("SELECT * FROM card")
+      return results.map do |result|
+        {
+          "id" => result["id"].to_i,
+          "player" => result["player"],
+          "team" => result["team"],
+          "image" => result["image"],
+          "position" => result["position"],
+          "batting_avg" => result["batting_avg"].to_i,
+          "card_owner" => result["card_owner"]
+        }
+      end #ends map do
+    end #ends self all
+
+end
