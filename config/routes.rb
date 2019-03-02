68e31c3ebda24426b054ac6f225ec5cc538ea7f9
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-03-02 11:45:32 -0500

    added person and card routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
 
   #ROUTES FOR PERSON CONTROLLER
   get '/person', to: 'person#index'
-
-
-
+  get '/person/:id', to: 'peson#show'
+  post '/person', to: 'person#create'
+  delete '/person/:id', to: 'person#delete'
+  put '/person/:id', to: 'person#update'
 
   #ROUTES FOR CARD CONTROLLER
   get '/card', to: 'card#index'
-
+  get '/card/:id', to: 'card#show'
+  post '/card', to: 'card#create'
+  delete '/card/:id', to: 'card#delete'
+  put '/card/:id', to: 'card#update'
 
 end
