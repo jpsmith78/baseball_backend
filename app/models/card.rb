e81bfc9c638428dbab34a1a7546dd65a9606f0be
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-03-02 14:10:48 -0500

    adjusted to work with heroku(I think)

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,11 @@
 class Card
 
-  DB = PG.connect({
-    :host =>'localhost',
-    :port =>5432,
-    :dbname =>"baseball_backend_development"
-    })
+  if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'baseball_backend_development')
+    end
 
     def self.all
       results = DB.exec("SELECT * FROM card")
